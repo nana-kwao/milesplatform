2f4d30611d9e2dfe292547b2d73d8c7b4a99ed42
Nana <99691633+nana-kwao@users.noreply.github.com>
2025-08-03 19:02:04 +0000

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https:nana-kwao.github.io/milesplatform/",
+  base: "https://nana-kwao.github.io/milesplatform/",
   server: {
     proxy: {
       "/api": {

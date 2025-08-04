2da4d02b083a5d4963070f1085473e3a9c2b44a2
Nana <99691633+nana-kwao@users.noreply.github.com>
2025-08-04 02:54:40 +0000

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://miles-ck5d.onrender.com/",
+  base: "/",
   server: {
     proxy: {
       "/api": {
